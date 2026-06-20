1c86e60d48b888af19adc2f10991135ce1c158e6
Kinetic <infstatemachines@proton.me>
2026-06-20 02:01:53 -0500

    Made flag label richtextlabel so copyable

--- ctf-programming-challenge/flag_gui/flag_gui.gd
+++ ctf-programming-challenge/flag_gui/flag_gui.gd
@@ -1,7 +1,7 @@
 extends Control
 
 @onready
-var flag_label: Label = %FlagLabel
+var flag_label: RichTextLabel = %FlagLabel
 
 func set_flag_label(string: String) -> void:
 	flag_label.text = string
