6c8c316e94e1f2d96640e6bd77bf4cb76eba8161
Kinetic <infstatemachines@proton.me>
2026-06-19 20:26:44 -0500

    Set variables to static types

--- ctf-programming-challenge/registration_gui/registration_gui.gd
+++ ctf-programming-challenge/registration_gui/registration_gui.gd
@@ -3,10 +3,10 @@ extends Control
 signal register_button_pressed(String)
 
 @onready
-var register_button = %RegisterBtn
+var register_button: Button = %RegisterBtn
 
 @onready
-var registration_text_input = %RegistrationTextInput
+var registration_text_input: LineEdit = %RegistrationTextInput
 
 func _ready() -> void:
 	register_button.pressed.connect(_on_RegisterBtn_pressed)
