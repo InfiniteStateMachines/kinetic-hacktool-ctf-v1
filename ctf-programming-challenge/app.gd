cc1022c1835d09fe2ca3c9abd036f571f6ca0da7
Kinetic <infstatemachines@proton.me>
2026-06-20 01:34:35 -0500

    Make secret hashed
    Removed plaintext secret.  Oops! Hope boss doesn't find out.

--- ctf-programming-challenge/app.gd
+++ ctf-programming-challenge/app.gd
@@ -7,7 +7,7 @@ var registration_gui: Control = $RegistrationGui
 var flag_gui: Control = $FlagGui
 
 var flag = [86,81,57,81,86,48,108,70,82,49,49,100,84,48,77,83,67,49,57,100,67,103,74,76,88,108,49,85,70,119,90,90,71,108,107,68,84,85,65,98,67,103,112,74,81,108,73,85,88,66,65,99,86,119,74,83,65,119,86,81]
-var secret = [51,99,49,48,50,54,51,50,45,98,48,102,100,45,52,100,101,102,45,56,55,101,99,45,55,50,102,52,51,54,99,100,100,48,55,100]
+var secret_hash = "9e9353ef7b493ad1d37dcb9ef303792eb2b891590c1dbe7f1528c68a4f7e4b39"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -23,7 +23,12 @@ func _on_register_button_pressed(registration_value: String) -> void:
 		flag_gui.set_visible(true)
 
 func validate_registration(registration_value) -> bool:
-	var ascii_chars = []
-	for c in registration_value:
-		ascii_chars.append(ord(c))
-	return true
+	var hash_ctx = HashingContext.new()
+	hash_ctx.start(HashingContext.HASH_SHA256)
+	hash_ctx.update(registration_value.to_utf8_buffer())
+	var hex = hash_ctx.finish().hex_encode()
+	
+	if hex == secret_hash:
+		return true
+	else:
+		return false
